a3ed80fa4ef909f4cce3123af53e16f80e0887f0
Nikita Grebnovs <nikita.grebnovs@lendsecured.com>
2021-06-19 17:01:19 +0300

    update

--- resources/views/montage.blade.php
+++ resources/views/montage.blade.php
@@ -18,6 +18,41 @@
     </div>
 @endsection
 @section('content')
+    <div class="bg-vs-yellow">
+    <div class="container-fluid">
+    <div class="justify-content-around row me-5 ms-auto" id="what-we-do-banner">
+        <div class="col-4 d-flex">
+            <img src="{{asset('assets/img/logoBuilder.png')}}" class="banner-img me-5 ms-5 mt-3 " alt="">
+            <div>
+                <h2>Design & Build <strong>property</strong></h2>
+                <p>If more than 12 columns are placed within a single row, each group of extra columns will, as one unit, wrap onto a new line.</p>
+                <a href="#">More</a>
+            </div>
+        </div>
+        <div class="col-4 d-flex">
+            <img src="{{asset('assets/img/logoBuilder.png')}}" class="banner-img me-5 ms-5 mt-3" alt="">
+            <div>
+                <h2>Design & Build <strong>property</strong></h2>
+                <p>If more than 12 columns are placed within a single row, each group of extra columns will, as one unit, wrap onto a new line.</p>
+                <a href="#">More</a>
+            </div>
+        </div>
+        <div class="col-4 d-flex">
+            <img src="{{asset('assets/img/logoBuilder.png')}}" class="banner-img me-5 ms-5 mt-3" alt="">
+            <div>
+                <h2>Design & Build <strong>property</strong></h2>
+                <p>If more than 12 columns are placed within a single row, each group of extra columns will, as one unit, wrap onto a new line.</p>
+                <a href="#">More</a>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+
+    </div>
+
 
     <div style="height: 70vh"></div>
 
